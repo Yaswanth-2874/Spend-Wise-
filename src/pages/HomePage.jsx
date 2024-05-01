a2beced42220b46d6694ca0ebecdd1eaeefb040e
Yaswanth <yryr2004@gmail.com>
2024-05-01 14:41:50 +0530

    Login page is functionally ready

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,7 +1,11 @@
 import React from "react";
+import { useLocation } from "react-router-dom";
 
 const HomePage = () => {
-  return <div>HomePage</div>;
+  const location = useLocation();
+  const userData = location.state;
+  console.log(userData);
+  return <div>{userData.email}</div>;
 };
 
 export default HomePage;
--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,39 +1,66 @@
 import React, { useState } from "react";
-import { Box, Button, TextField, styled } from "@mui/material";
+import { Button, TextField } from "@mui/material";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 const LoginPage = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-
-
+  const textBoxStyle = {
+    width: "250px",
+  };
+  const buttonStyle = {
+    height: "40px",
+  };
+  const navigate = useNavigate();
+  const handleClick = () => {
+    axios
+      .post("http://localhost:4000/users/login", {
+        email: email,
+        password: password,
+      })
+      .then((response) => {
+        if (!response.data) {
+          alert("Something went wrong");
+        } else if (response.data.status === "bad") {
+          alert("Incorrect credentials");
+        } else {
+          navigate("/", { state: response.data });
+        }
+      })
+      .catch((e) => console.log("Not able to login due to ", e));
+  };
   return (
-    <div>
-      <Box
-        component="form"
-        sx={{
-          "& .MuiTextField-root": { m: 1, width: "25ch" },
-        }}
-        noValidate
-        autoComplete="off"
-      >
-        <TextField
-          id="outlined-basic"
-          label="Email"
-          variant="outlined"
-          onChange={(e) => setEmail(e.target.value)}
-        />
-        <TextField
-          id="outlined-password-input"
-          label="Password"
-          type="password"
-          onChange={(e) => setPassword(e.target.value)}
-          autoComplete="current-password"
-        />
-        <Button variant="outlined" href="/">
-          Link
-        </Button>
-      </Box>
-    </div>
+    <form
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        gap: "20px",
+        padding: "20px 20px",
+        backgroundColor: "rgb(255, 255, 220)",
+        borderRadius: "30px",
+      }}
+    >
+      <TextField
+        id="outlined-basic"
+        label="Email"
+        variant="outlined"
+        style={textBoxStyle}
+        onChange={(e) => setEmail(e.target.value)}
+      />
+      <TextField
+        id="outlined-password-input"
+        label="Password"
+        type="password"
+        onChange={(e) => setPassword(e.target.value)}
+        autoComplete="current-password"
+        style={textBoxStyle}
+      />
+      <Button variant="outlined" onClick={handleClick} style={buttonStyle}>
+        Log In
+      </Button>
+    </form>
   );
 };
 
